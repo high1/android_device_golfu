4702645e49672223dc794d115c22d47a39e7f5c3
high1 <radomir.simic@gmail.com>
2015-01-19 23:39:03 +0100

    Minor updates for generic mk files

--- cm.mk
+++ cm.mk
@@ -5,21 +5,15 @@ TARGET_SCREEN_WIDTH  := 480
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/htc/golfu/golfu.mk)
 
 # Product name
 PRODUCT_DEVICE := golfu
 PRODUCT_NAME := cm_golfu
-PRODUCT_BRAND := htc_europe
+PRODUCT_BRAND := HTC
 PRODUCT_MODEL := HTC Desire C
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_RELEASE_NAME := Desire C
-PRODUCT_VERSION_DEVICE_SPECIFIC := -golfu
+PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
 PRODUCT_CHARACTERISTICS := phone
-
-#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=golfu BUILD_ID=KTU84Q BUILD_FINGERPRINT=htc_europe/htc_golfu/golfu:4.4.4/KTU84Q/87995:user/release-keys
-#PRIVATE_BUILD_DESC="1.35.401.1 CL62541 release-keys"
